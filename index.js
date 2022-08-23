b7afc240954f96af5a5a9b42feebe760996c5b29
sevc4ik <31854320+sevc4ik@users.noreply.github.com>
2022-08-23 22:42:42 +0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/router.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5000;
 const DB_URL = `mongodb+srv://whatMeme:MEstb2VWB7Zm8Gt5@cluster0.39nekth.mongodb.net/MemeBase?retryWrites=true&w=majority`;
@@ -8,7 +9,7 @@ const DB_URL = `mongodb+srv://whatMeme:MEstb2VWB7Zm8Gt5@cluster0.39nekth.mongodb
 const app = express();
 
 app.use(express.json());
-
+app.use(cors())
 app.use("/api", router);
 
 async function startApp() {
