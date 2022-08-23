32a0c7ad89913bd968bf7d339c84836b6587f10b
sevc4ik <31854320+sevc4ik@users.noreply.github.com>
2022-08-23 15:07:24 +0300

    feat: add startapp

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/router.js";
+
+const PORT = process.env.PORT || 5000;
+const DB_URL = `mongodb+srv://whatMeme:MEstb2VWB7Zm8Gt5@cluster0.39nekth.mongodb.net/MemeBase?retryWrites=true&w=majority`;
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api", router);
+
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    app.listen(PORT, () => console.log("Server started on port " + PORT));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+startApp();
