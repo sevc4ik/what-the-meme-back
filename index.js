9e772e515efe1367f1c927e9ca19fcdd53a59f5d
sevc4ik <31854320+sevc4ik@users.noreply.github.com>
2022-09-06 00:52:43 +0300

    feat: add disconnect handler

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ import router from "./routes/router.js";
 import cors from "cors";
 import WSS from "express-ws";
 
+import RoomService from "./service/roomService.js";
+import { handlerDisconnectChat } from "./websocket/chat.js";
+import { updateRoomState, getRoomMembers } from "./websocket/room.js";
+import { handleGameState } from "./websocket/state.js";
+import { updateSituation } from "./websocket/situation.js";
+
 const PORT = process.env.PORT || 5000;
-const DB_URL = `mongodb+srv://whatMeme:MEstb2VWB7Zm8Gt5@cluster0.39nekth.mongodb.net/MemeBase?retryWrites=true&w=majority`;
+const DB_URL =
+  process.env.URL ||
+  `mongodb+srv://whatMeme:MEstb2VWB7Zm8Gt5@cluster0.39nekth.mongodb.net/MemeBase?retryWrites=true&w=majority`;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use("/api", router);
 
+const clientsMemeData = [];
+const clientsScoreData = [];
 
 const WSServer = WSS(app);
 const aWss = WSServer.getWss();
@@ -28,18 +38,26 @@ async function startApp() {
     console.log(e);
   }
 }
-
 app.ws("/", (ws, req) => {
   ws.on("message", (msg) => {
     msg = JSON.parse(msg);
-  console.log(msg)
-    switch (msg.method) {
-      case "connection":
-        connectionHandler(ws, msg);
-        break;
-      case "message":
-        broadcastConnection(ws, msg);
-        break;
+    if (msg.id) {
+      switch (msg.method) {
+        case "connection":
+          connectionHandler(ws, msg);
+          break;
+        case "message":
+          broadcastConnection(ws, msg);
+          break;
+      }
+    }
+  });
+  ws.on("close", async (msg, reason) => {
+    if (reason) {
+      const [userId, roomId] = reason.split(" ");
+      if (userId && roomId) {
+        handlerDisconnectChat(userId, roomId, ws);
+      }
     }
   });
 });
