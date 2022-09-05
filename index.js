a9589ca238fdf384b6eec91805acef0c5d1bc9cd
sevc4ik <31854320+sevc4ik@users.noreply.github.com>
2022-09-06 01:10:04 +0300

    feat: add timer

--- index.js
+++ index.js
@@ -217,4 +217,119 @@ const broadcastConnection = (ws, msg) => {
   });
 };
 
+const memeSelectTimer = async (roomId, seconds, ws, state) => {
+  const timer = setInterval(async function () {
+    if (time <= 0) {
+      clearInterval(timer);
+      broadcastConnection(ws, createObjTimer(roomId, time));
+
+      const { currentClientMemeIndex, currentMemeClient } =
+        getCurrentClientMemeData(clientsMemeData, roomId);
+      const { currentScoreClient, currentScoreClientIndex } =
+        getCurrentClientScoreData(clientsScoreData, roomId);
+
+      if (getAmountConnectedWSById(roomId)) {
+        switch (state) {
+          case "Голосование": {
+            const room = await RoomService.getRoom(roomId);
+            const { gameProgress } = room;
+
+            gameProgress.userMemePick = [];
+
+            if (currentMemeClient && clientsMemeData[currentClientMemeIndex])
+              clientsMemeData[currentClientMemeIndex].userMemePick.length = 0;
+
+            await RoomService.updateRoom(roomId, { gameProgress });
+            await handleGameState(roomId, "Считаем баллы", ws);
+            memeSelectTimer(roomId, 5, ws, "Считаем баллы");
+            break;
+          }
+          case "Выберите мем": {
+            await handleGameState(roomId, "Собираем мемы", ws);
+            memeSelectTimer(roomId, 2, ws, "Собираем мемы");
+            break;
+          }
+          case "Считаем баллы": {
+            const room = await RoomService.getRoom(roomId);
+            const { gameProgress, users } = room;
+            if (clientsScoreData[currentScoreClientIndex]) {
+              gameProgress.userScore =
+                clientsScoreData[currentScoreClientIndex].userScore;
+              users.forEach((user, index) => {
+                const tempUserData = gameProgress.userScore.find(
+                  (secondUser) => secondUser.id === user.id
+                );
+                if (tempUserData) {
+                  if (typeof user["score"] !== "undefined") {
+                    users[index].score += Number(tempUserData.score);
+                  } else {
+                    users[index]["score"] = tempUserData.score;
+                  }
+                }
+              });
+              await RoomService.updateRoom(roomId, { gameProgress });
+              await RoomService.updateRoom(roomId, { users });
+
+              broadcastConnection(ws, {
+                method: "score",
+                content: users,
+                id: roomId,
+              });
+            }
+
+            await handleGameState(roomId, "Следующий раунд стартует через", ws);
+            memeSelectTimer(roomId, 4, ws, "Следующий раунд стартует через");
+            break;
+          }
+          case "Результаты": {
+            await handleGameState(roomId, "Следующий раунд стартует через", ws);
+            await updateSituation(ws, roomId);
+            memeSelectTimer(roomId, 3, ws, "Следующий раунд стартует через");
+            break;
+          }
+          case "Собираем мемы": {
+            const room = await RoomService.getRoom(roomId);
+            const { gameProgress } = room;
+
+            if (clientsMemeData[currentClientMemeIndex]) {
+              gameProgress.userMemePick =
+                clientsMemeData[currentClientMemeIndex].userMemePick;
+              await RoomService.updateRoom(roomId, { gameProgress });
+              broadcastConnection(ws, {
+                method: "meme",
+                content: clientsMemeData[currentClientMemeIndex].userMemePick,
+                id: roomId,
+              });
+            }
+            await handleGameState(roomId, "Голосование", ws);
+            await updateSituation(ws, roomId);
+            memeSelectTimer(roomId, 6, ws, "Голосование");
+            break;
+          }
+          case "Следующий раунд стартует через": {
+            const room = await RoomService.getRoom(roomId);
+            const { gameProgress } = room;
+
+            gameProgress.userScore = [];
+
+            if (typeof currentScoreClientIndex !== "undefined") {
+              clientsScoreData[currentScoreClientIndex].userScore.length = 0;
+            }
+            await RoomService.updateRoom(roomId, { gameProgress });
+            await handleGameState(roomId, "Выберите мем", ws);
+            memeSelectTimer(roomId, 7, ws, "Выберите мем");
+            break;
+          }
+        }
+      } else {
+        clearInterval(timer);
+      }
+    } else {
+      broadcastConnection(ws, createObjTimer(roomId, time));
+      --time;
+    }
+  }, 1000);
+  let time = seconds;
+};
+
 startApp();
